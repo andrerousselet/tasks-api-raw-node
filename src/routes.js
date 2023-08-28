7dd5f6cdbc6086954fad69307f0401925ef8f8c0
Andre Rousselet <andre098@hotmail.com>
2023-08-28 17:10:36 -0300

    feat(routes-and-db): add insert method in db and POST /tasks endpoint to create new task
    use randomUUID from node

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-// import { randomUUID } from "node:crypto";
+import { randomUUID } from "node:crypto";
 import { Database } from "./database.js";
 
 const db = new Database();
@@ -10,6 +10,24 @@ export const routes = [
     handler: (req, res) => {
       const tasks = db.select("tasks");
       return res.end(JSON.stringify(tasks));
+    },
+  },
+  {
+    method: "POST",
+    path: "/tasks",
+    handler: (req, res) => {
+      const { title, description } = req.body;
+      const now = new Date();
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: now,
+        updated_at: now,
+      };
+      db.insert("tasks", task);
+      return res.writeHead(201).end("News task created!");
     },
   },
 ];
